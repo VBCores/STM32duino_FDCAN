3e8afa1c54368312aad4dcf3f619c509e842e96d
Dmitry <dmivaka@yandex.ru>
2023-05-18 13:01:26 +0300

    Added CAN frames reception in polling mode. Switched to extended IDs.

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -10,11 +10,12 @@
 FDCAN_HandleTypeDef     hfdcan1;
 
 static void MX_FDCAN1_Init(void);
-void CANFD_Start( uint8_t id);
+void CANFD_Start(void);
 
 void setup()
 {
-  pinMode(PC13, OUTPUT);
+  pinMode(PD2, OUTPUT);
+  pinMode(PA5, OUTPUT);
   Serial.begin(115200);
   
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -34,7 +35,7 @@ void setup()
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   MX_FDCAN1_Init();
-  CANFD_Start( 10 );
+  CANFD_Start();
 }
 
 void loop() 
@@ -46,7 +47,7 @@ void loop()
     
     // Add message to Tx FIFO 
     TxHeader.Identifier = 0x01;
-    TxHeader.IdType = FDCAN_STANDARD_ID;
+    TxHeader.IdType = FDCAN_EXTENDED_ID;
     TxHeader.TxFrameType = FDCAN_DATA_FRAME;
     TxHeader.DataLength = FDCAN_DLC_BYTES_8;
     TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
@@ -54,80 +55,49 @@ void loop()
     TxHeader.FDFormat = FDCAN_FD_CAN;
     TxHeader.TxEventFifoControl = FDCAN_STORE_TX_EVENTS;
     TxHeader.MessageMarker = 0x00;
-    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, data) != HAL_OK)
-    {
-      Error_Handler();
-    }
+    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, data) != HAL_OK){ Error_Handler(); }
 
-    digitalWrite(PC13, !digitalRead(PC13));
+    digitalWrite(PD2, !digitalRead(PD2));
+  }
+
+  while( HAL_FDCAN_GetRxFifoFillLevel(&hfdcan1, FDCAN_RX_FIFO0) > 0 )
+  {
+    FDCAN_RxHeaderTypeDef Header;
+    uint8_t RxData[64];
+    
+    if (HAL_FDCAN_GetRxMessage(&hfdcan1, FDCAN_RX_FIFO0, &Header, RxData) != HAL_OK){ Error_Handler(); }
+    
+    digitalWrite(PA5, !digitalRead(PA5));
   }
   delay(1000);
 }
 
 
-void CANFD_Start( uint8_t id)
+void CANFD_Start( void )
 {
-  //mapper(bruh, bruhhh, mapp);
-
-  uint32_t Mask = 0x10 * (uint32_t)id;
-  
-  // Filter for messages from master to this dedicated device. 
+  // Accepts all frames
   FDCAN_FilterTypeDef sFilterConfig;  
-  sFilterConfig.IdType = FDCAN_STANDARD_ID;
-  sFilterConfig.FilterIndex = 1;
-  sFilterConfig.FilterType = FDCAN_FILTER_RANGE;
-  sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-  sFilterConfig.FilterID1 = Mask + 0x08;
-  sFilterConfig.FilterID2 = Mask + 0x10;
-  if (HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  // Filter for broadcast messages from master.
-  sFilterConfig.IdType = FDCAN_STANDARD_ID;
+  sFilterConfig.IdType = FDCAN_EXTENDED_ID;
   sFilterConfig.FilterIndex = 0;
   sFilterConfig.FilterType = FDCAN_FILTER_RANGE;
   sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-  sFilterConfig.FilterID1 = 0x08;
-  sFilterConfig.FilterID2 = 0x10;
-  if (HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  sFilterConfig.FilterID1 = 0x0;
+  sFilterConfig.FilterID2 = 0x1FFFFFFF;
+  if (HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK){ Error_Handler(); }  
 
   /* Configure global filter:
      Filter all remote frames with STD and EXT ID
      Reject non matching frames with STD ID and EXT ID */
-  if (HAL_FDCAN_ConfigGlobalFilter(&hfdcan1, FDCAN_REJECT, FDCAN_REJECT, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_FDCAN_ConfigGlobalFilter(&hfdcan1, FDCAN_REJECT, FDCAN_REJECT, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE) != HAL_OK){ Error_Handler(); }
 
-  // Activate Rx FIFO 0 new message notification
-  if (HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  
   /* Configure and enable Tx Delay Compensation, required for BRS mode.
      TdcOffset default recommended value: DataTimeSeg1 * DataPrescaler
      TdcFilter default recommended value: 0 */
-  if (HAL_FDCAN_ConfigTxDelayCompensation(&hfdcan1, 5, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  
-  if (HAL_FDCAN_EnableTxDelayCompensation(&hfdcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_FDCAN_ConfigTxDelayCompensation(&hfdcan1, 5, 0) != HAL_OK){ Error_Handler(); }
+  if (HAL_FDCAN_EnableTxDelayCompensation(&hfdcan1) != HAL_OK){ Error_Handler(); }
 
   //2 Start FDCAN controller (continuous listening CAN bus)
-  if (HAL_FDCAN_Start(&hfdcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_FDCAN_Start(&hfdcan1) != HAL_OK){ Error_Handler(); }
 
   Serial.println("Started FDCAN");
 }
@@ -153,36 +123,26 @@ extern "C" void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){ Error_Handler(); }
+
   /// Initializes the CPU, AHB and APB busses clocks 
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK){ Error_Handler(); }
+
   /// Initializes the peripherals clocks 
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2
-                              |RCC_PERIPHCLK_ADC12|RCC_PERIPHCLK_FDCAN;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_ADC12|RCC_PERIPHCLK_FDCAN;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
   PeriphClkInit.FdcanClockSelection = RCC_FDCANCLKSOURCE_PCLK1;
   PeriphClkInit.Adc12ClockSelection = RCC_ADC12CLKSOURCE_SYSCLK;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK){ Error_Handler(); }
 }
 
 static void MX_FDCAN1_Init(void)
@@ -198,7 +158,7 @@ static void MX_FDCAN1_Init(void)
   hfdcan1.Instance = FDCAN1;
   hfdcan1.Init.ClockDivider = FDCAN_CLOCK_DIV2;
   hfdcan1.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
-  hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;//FDCAN_MODE_EXTERNAL_LOOPBACK;
+  hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
   hfdcan1.Init.AutoRetransmission = ENABLE;
   hfdcan1.Init.TransmitPause = ENABLE;
   hfdcan1.Init.ProtocolException = DISABLE;
@@ -210,13 +170,11 @@ static void MX_FDCAN1_Init(void)
   hfdcan1.Init.DataSyncJumpWidth = 4;
   hfdcan1.Init.DataTimeSeg1 = 5;
   hfdcan1.Init.DataTimeSeg2 = 4;
-  hfdcan1.Init.StdFiltersNbr = 2;
-  hfdcan1.Init.ExtFiltersNbr = 0;
+  hfdcan1.Init.StdFiltersNbr = 0;
+  hfdcan1.Init.ExtFiltersNbr = 1;
   hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
-  if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK){ Error_Handler(); }
+
   /* USER CODE BEGIN FDCAN1_Init 2 */
   Serial.println("Initialized FDCAN");
   /* USER CODE END FDCAN1_Init 2 */
